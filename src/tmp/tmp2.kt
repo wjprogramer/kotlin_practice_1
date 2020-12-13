b0222deb940423be25404ef37e38d4958e56b7e0
Jay <_@_.com>
2020-12-13 10:15:07 +0800

    no message

--- src/tmp/tmp2.kt
+++ src/tmp/tmp2.kt
@@ -1,17 +1,2 @@
 package tmp
 
-fun dayOfYear1(date: String): Int {
-    val year = date.substring(0, 4)
-    val month = date.substring(5, 7)
-    val day = date.substring(8, 10)
-
-    
-
-    return 0
-}
-
-fun main() {
-    println(dayOfYear1("2020-01-09"))
-    println(dayOfYear1("2020-01-09"))
-    println(dayOfYear1("2020-01-09"))
-}
